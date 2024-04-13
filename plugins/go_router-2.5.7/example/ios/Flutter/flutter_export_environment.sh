3bb5cc22725f6c3a055c6710cd3639b9a46fda2e
xiaoliu19890 <xiaoliu19890@gmail.com>
2024-04-13 15:50:04 +0800

    fix fix fix

--- plugins/go_router-2.5.7/example/ios/Flutter/flutter_export_environment.sh
+++ plugins/go_router-2.5.7/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac/Desktop/gdcm/plugins/go_router-2.5.7/example"
+export "FLUTTER_ROOT=/Users/mac/fvm/versions/2.10.0"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/Desktop/awjq/plugins/go_router-2.5.7/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
