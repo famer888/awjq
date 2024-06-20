e961c0646309bb6cdf69c561d32b60cf53fb4e30
mac <>
2024-06-20 10:36:37 +0700

    fxifxfx

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ void main() async {
         "bundleId": packageInfo.packageName,
         "version": packageInfo.version,
         "oauth_type": "android",
-        // "build_affcode": "bFqvJ",
+        // "build_affcode": "G8EM",
       };
     } else {
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
