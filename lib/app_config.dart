e19827ced9f8327e4db9fccbbdcba9f23af24e18
jiangyi <>
2024-10-21 19:30:44 +0700

    换api域名

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -17,7 +17,7 @@ class BuildConfig {
           'https://d2i7v9uktjmxdx.cloudfront.net/api.php',
         ]
       : [
-          'https://d225ilsto3novd.cloudfront.net/api.php',
+          'https://d3myclmpa1s8p.cloudfront.net/api.php',
           'https://bpi4.uh6vg7yu.com/api.php',
         ];
 
