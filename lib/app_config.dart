b6860b50fc3e6970496aacefd09f4e683cc9b2b6
jiangyi <>
2025-01-09 19:05:16 +0700

    replace web api lines

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -14,8 +14,8 @@ class BuildConfig {
   /// 备用接口线路
   static final apiLines = kIsWeb
       ? [
-          'https://bpi3.mxpuqgdg.cc/api.php',
-          'https://lnx.mmsnmdkc.cc/api.php',
+          'https://bpi3.esfmekjl.net/api.php',
+          'https://lnx.esfmekjl.net/api.php'
         ]
       : [
           'https://d3myclmpa1s8p.cloudfront.net/api.php',
