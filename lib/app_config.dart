db59f9ea5bd2652198473bf381b6bb6d8a0349b4
jiangyi <>
2025-04-23 11:20:51 +0700

    replace api domain

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -14,12 +14,13 @@ class BuildConfig {
   /// 备用接口线路
   static final apiLines = kIsWeb
       ? [
-          'https://bpi3.esfmekjl.net/api.php',
-          'https://lnx.esfmekjl.net/api.php'
+          'https://bpi3.fycpmqg.xyz/api.php',
+          'https://lnx.fycpmqg.xyz/api.php',
         ]
       : [
           'https://d3myclmpa1s8p.cloudfront.net/api.php',
-          'https://bpi4.uh6vg7yu.com/api.php',
+          'https://bpi4.fycpmqg.xyz/api.php',
+          'https://d2i7v9uktjmxdx.cloudfront.net/api.php',
         ];
 
   /// 备用线路
